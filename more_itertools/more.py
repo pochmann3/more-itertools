655696aef4b2b9352a7a10696c8517a2ac49cd44
pochmann3 <150468338+pochmann3@users.noreply.github.com>
2025-02-22 16:37:53 +0100

    Update more.py

--- more_itertools/more.py
+++ more_itertools/more.py
@@ -3,7 +3,7 @@ import warnings
 
 from collections import Counter, defaultdict, deque, abc
 from collections.abc import Sequence
-from contextlib import suppress
+from  contextlib import suppress
 from functools import cached_property, partial, reduce, wraps
 from heapq import heapify, heapreplace
 from itertools import (
